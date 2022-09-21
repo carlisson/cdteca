5505127f9ab41eac6c082dbbe122a067b48210a0
Cárlisson Galdino <carlisson@nti.ufal.br>
2022-09-21 09:11:22 -0300

    Getopts included

--- cdteca.py
+++ cdteca.py
@@ -1,13 +1,29 @@
 #!/usr/bin/python3
 from yaml import safe_load
-import os.path
+import os.path, getopt, sys
 
-version = "0.0dev2"
+version = "0.0dev3"
 confile = os.path.dirname(__file__) + "/config.yaml"
 
-if os.path.exists(confile):
-    with open(confile, 'r') as file:
-        cdconf = safe_load(file)
-        print(cdconf)
-else:
-    print("Configuration file " + confile + " not found.")
+def usage():
+    print("Usage instructions.")
+
+def main():
+
+    if os.path.exists(confile):
+        with open(confile, 'r') as file:
+            cdconf = safe_load(file)
+            #print("Loaded: " + str(cdconf))
+    else:
+        print("Configuration file " + confile + " not found.")
+        sys.exit(1)
+
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "hsifu", ["help", "status", "start", "stop", "update"] )
+    except getopt.GetoptError as err:
+        print(err)
+        usage()
+        sys.exit(2)
+
+if __name__ == "__main__":
+    main()
