045e96a9f04adcdebcd268d7c9bad03e6fd927a7
Cárlisson Galdino <carlisson@nti.ufal.br>
2022-09-21 08:50:01 -0300

    Fisrt steps to load config.yaml file

--- cdteca.py
+++ cdteca.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
+from yaml import safe_load
+import os.path
 
-version = "0.0dev1"
+version = "0.0dev2"
+confile = os.path.dirname(__file__) + "/config.yaml"
+
+if os.path.exists(confile):
+    with open(confile, 'r') as file:
+        cdconf = safe_load(file)
+        print(cdconf)
+else:
+    print("Configuration file " + confile + " not found.")
